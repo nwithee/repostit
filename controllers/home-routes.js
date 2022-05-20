8e25a4c6413fee5cba5316acf605ff882135f86d
nzwithee@gmail.com <Cartoon#01>
2022-05-19 19:23:31 -0500

    updates to UI on comments

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -72,7 +72,7 @@ router.get('/post/:id', (req, res) => {
     include: [
       {
         model: Comment,
-        attributes: ['id', 'comment_text', 'post_id', 'user_id'],
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
         include: {
           model: User,
           attributes: ['username']
